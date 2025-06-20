4e06ce1dd125ea32efa74582455ad53013f2ceac
Louie Veleski <louieveleski@Louies-iMac-2.local>
2025-06-20 20:23:28 +1000

    CRITICAL: Add NextAuth env vars to next.config.js - Fix Google OAuth Error 401

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-   module.exports = {
-     images: {
-       domains: ['res.cloudinary.com'],
-     },
-   }
+module.exports = {
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  },
+}
