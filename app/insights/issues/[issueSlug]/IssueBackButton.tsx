7c21ba47c76c728626ee4abb26a44ded655d4df7
Helfi <helfiweb@gmail.com>
2025-09-28 17:38:55 +1000

    Route issue back button to insights list

--- app/insights/issues/[issueSlug]/IssueBackButton.tsx
+++ app/insights/issues/[issueSlug]/IssueBackButton.tsx
@@ -1,17 +1,14 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 export default function IssueBackButton() {
-  const router = useRouter()
-
   return (
-    <button
-      type="button"
-      onClick={() => router.back()}
+    <Link
+      href="/insights"
       className="inline-flex items-center gap-2 text-helfi-green font-semibold"
     >
       <span className="text-lg">←</span> Back
-    </button>
+    </Link>
   )
 }
